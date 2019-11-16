e28776e0b81e664dd6b9099e3ecca38727659c2d
Dmitry Kuznetsov <dimakuzn@gmail.com>
2019-11-16 11:51:45 +0300

    Updates

---
+++ precache-manifest.7e18101a059cc0f21c102c637ebc1119.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e60f0eadfe73971b5f15ef1d5aa99d56",
+    "url": "/moove/index.html"
+  },
+  {
+    "revision": "a4381c7c014d03d86c23",
+    "url": "/moove/static/css/2.266626f9.chunk.css"
+  },
+  {
+    "revision": "aeb9fb4fb1557085ef41",
+    "url": "/moove/static/css/main.981f7211.chunk.css"
+  },
+  {
+    "revision": "a4381c7c014d03d86c23",
+    "url": "/moove/static/js/2.b918cb8f.chunk.js"
+  },
+  {
+    "revision": "aeb9fb4fb1557085ef41",
+    "url": "/moove/static/js/main.8bcb42a3.chunk.js"
+  },
+  {
+    "revision": "99e4351cd3fc007c791b",
+    "url": "/moove/static/js/runtime-main.8f99bb52.js"
+  },
+  {
+    "revision": "4e1ec8403d903dc514271d7328fbdeb3",
+    "url": "/moove/static/media/persik.4e1ec840.png"
+  }
+]);
