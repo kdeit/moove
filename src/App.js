62235c1f23254c8ab11d29a379051dbb27be1abd
Dmitry Kuznetsov <dimakuzn@gmail.com>
2019-11-18 22:31:04 +0300

    Share

--- src/App.js
+++ src/App.js
@@ -37,7 +37,7 @@ const App = () => {
 	}
 
 	const share = () => {
-		connect.send("VKWebAppShowWallPostBox", { "message": "Уже через " + data.time + " даже " + data.who + " сможет " + data.what + " лучше меня. Попробуй.", "attachments": "https://vk.com/app7210481,photo39721559_457239052" });
+		connect.send("VKWebAppShowWallPostBox", { "message": "Уже через " + data.time + " даже " + data.who + " сможет " + data.what + " лучше меня. Запишись на программу MOOVE пока не поздно! https://common.skolkovo.ru/moove-by-skolkovo-mts/", "attachments": "https://vk.com/app7210481,photo39721559_457239052" });
 	}
 
 	const [fetchedUser, setUser] = useState(null);
